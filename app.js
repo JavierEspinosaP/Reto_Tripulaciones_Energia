7a7d1c818a53804138443e281fe80718166dfa8b
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-10-10 12:15:11 +0200

    initial_config

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+
+const cowsay = require('cowsay2');
+const owl = require('cowsay2/cows/owl');
+const path = require('path');
+
+
+const app = express()
+
+
+const port = process.env.PORT || 3000
+app.use(express.static('public'));
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+
+//Owl say you if server works
+app.listen(port, () => {
+    console.log(cowsay.say(`Server working on http://localhost:${port}`, { cow: owl }))
+  });
