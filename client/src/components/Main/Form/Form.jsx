469c5ae7065bf7566a79b9a72e45d410d98870ec
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-10-15 12:12:08 +0200

    working-on-form

--- client/src/components/Main/Form/Form.jsx
+++ client/src/components/Main/Form/Form.jsx
@@ -6,6 +6,7 @@ import List from '@mui/material/List';
 import Button from '@mui/material/Button';
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import { useDebouncedCallback } from 'use-debounce';
 
 // const items = [
 //   {value: 'apple'},
@@ -17,92 +18,151 @@ import { Link } from "react-router-dom";
 
 const Form = () => {
 
-  const [pokData, setPokData] = useState([])
+  const [brandData, setBrandData] = useState([])
+  const [modelData, setModelData] = useState([])
+  const [searchBrandData, setSearchBrandData] = useState([])
+  const [searchModelData, setSearchModelData] = useState([])
+  const [data, setData] = useState([])
 
-  useEffect(() => {  
+  // setData({
+  //   brand: searchBrandData,
+  //   model: searchModelData
+  // }) 
+
+  useEffect(() => {
 
     async function fetchData() {
       try {
 
-        const resDemo = await axios.get("http://desafioapitest-env.eba-kma62rdj.us-east-2.elasticbeanstalk.com/category?category=campanas",{
+        const resDemo = await axios.get("http://desafioapitest-env.eba-kma62rdj.us-east-2.elasticbeanstalk.com/category?category=campanas", {
           headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-        },})
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+          },
+        })
 
-        // console.log(resDemo);
+        setBrandData(await resDemo.data.Brand)
+        setModelData(await resDemo.data.Model_by_brand)
 
-        // const models = await resDemo.data.Brand
-
-        // const labels = await resPok.data.results
-
-        // const m = await pokemons.map(n => n.name)
-
-        // setPokData(names)
+        // console.log(resDemo.data.Model_by_brand['whirlpool']);
       }
       catch (e) {
-        setPokData([])
+        setBrandData(["error"])
+        setModelData(["error"])
       }
     }
     fetchData()
 
   }, [])
 
+  // setModelData(await resDemo.data.Model_by_brand[searchBrandData])
+
+
 
   return (
     <div className="formContainer">
-<Link   to={"/"}><Button className="btn-back" variant="contained">Volver</Button></Link>
-    <Downshift
-      onChange={(selection) =>
-        alert(selection ? `You selected ${selection}` : 'Selection Cleared')
-      }
-      itemToString={(item) => (item ? item : '')}
-    >
-      {({
-        getInputProps,
-        getItemProps,
-        getMenuProps,
-        getLabelProps,
-        getToggleButtonProps,
-        inputValue,
-        highlightedIndex,
-        selectedItem,
-        isOpen,
-      }) => (
-        <div style={comboboxStyles}>
-          <label {...getLabelProps()}>Enter a pokemon:</label>
-          <Input {...getInputProps()} />
-          <Button {...getToggleButtonProps()} aria-label={'toggle menu'}>
-            &#8595;
-          </Button>
-          <ul {...getMenuProps()} style={menuStyles}>
-            {isOpen &&
-              // items aqui es donde se cargan los datos para el autocompletado
-              pokData
-                .filter((item) => !inputValue || item.includes(inputValue.toLowerCase()))
-                .map((item, index) => (
-                  <List
-                    {...getItemProps({
-                      key: `${item}${index}`,
-                      item,
-                      index,
-                      style: {
-                        backgroundColor:
-                          highlightedIndex === index ? 'lightgray' : 'white',
-                        fontWeight: selectedItem === item ? 'bold' : 'normal',
-                      },
-                    })}
-                  >
-                    {item}
-                  </List>
-                  
-                ))}
-          </ul>
-        </div>
-      )}
-    </Downshift>      
+
+      <Link to={"/"}><Button className="btn-back" variant="contained">Volver</Button></Link>
+
+      {searchBrandData.length == 0 ? <Downshift
+        onChange={(selection) => selection ? setSearchBrandData(selection) : null}
+        itemToString={(item) => (item ? item : '')}
+      >
+
+        {({
+          getInputProps,
+          getItemProps,
+          getMenuProps,
+          getLabelProps,
+          getToggleButtonProps,
+          inputValue,
+          highlightedIndex,
+          selectedItem,
+          isOpen,
+        }) => (
+          <div style={comboboxStyles}>
+            <label {...getLabelProps()}>Introduce marca:</label>
+            <Input {...getInputProps()} />
+            <Button {...getToggleButtonProps()} aria-label={'toggle menu'}>
+              &#8595;
+            </Button>
+            <ul {...getMenuProps()} style={menuStyles}>
+              {isOpen &&
+                // items aqui es donde se cargan los datos para el autocompletado
+                brandData
+                  .filter((item) => !inputValue || item.includes(inputValue.toLowerCase()))
+                  .map((item, index) => (
+                    <List
+                      {...getItemProps({
+                        key: `${item}${index}`,
+                        item,
+                        index,
+                        style: {
+                          backgroundColor:
+                            highlightedIndex === index ? 'lightgray' : 'white',
+                          fontWeight: selectedItem === item ? 'bold' : 'normal',
+                        },
+                      })}
+                    >
+                      {item}
+                    </List>
+
+                  ))}
+            </ul>
+          </div>
+        )}
+      </Downshift> : <Downshift
+        onChange={(selection) => selection ? setSearchModelData(selection) : null}
+
+        itemToString={(item) => (item ? item : '')}
+      >
+
+        {({
+          getInputProps,
+          getItemProps,
+          getMenuProps,
+          getLabelProps,
+          getToggleButtonProps,
+          inputValue,
+          highlightedIndex,
+          selectedItem,
+          isOpen,
+        }) => (
+          <div style={comboboxStyles}>
+            <label {...getLabelProps()}>Introduce modelo:</label>
+            <Input {...getInputProps()} />
+            <Button {...getToggleButtonProps()} aria-label={'toggle menu'}>
+              &#8595;
+            </Button>
+            <ul {...getMenuProps()} style={menuStyles}>
+              {isOpen &&
+                // items aqui es donde se cargan los datos para el autocompletado
+                modelData[searchBrandData]
+                  .filter((item) => !inputValue || item.includes(inputValue.toLowerCase()))
+                  .map((item, index) => (
+                    <List
+                      {...getItemProps({
+                        key: `${item}${index}`,
+                        item,
+                        index,
+                        style: {
+                          backgroundColor:
+                            highlightedIndex === index ? 'lightgray' : 'white',
+                          fontWeight: selectedItem === item ? 'bold' : 'normal',
+                        },
+                      })}
+                    >
+                      {item}
+                    </List>
+
+                  ))}
+            </ul>
+          </div>
+        )}
+      </Downshift>}
+
     </div>
-    
+
   )
 }
 
