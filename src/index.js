986e7b986304d1cd7cd8ec62672a9f8106a4f4d9
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-10-17 23:39:48 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TagManager from 'react-gtm-module'
+ 
+const tagManagerArgs = {
+    gtmId: 'GTM-KX48BGN'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
