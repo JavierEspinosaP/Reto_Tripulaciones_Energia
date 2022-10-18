c5014dda72c2994e9467c3c66661e4b09cc32291
Nacho-sf <serranof.ignacio@gmail.com>
2022-10-18 13:16:38 +0200

    home/style

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, {useState} from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/styles.scss';
 import Main from './components/Main'
-import Header from './components/Header'
-import Footer from './components/Footer'
 import { dataContext }  from './context/dataContext';
 
 
@@ -21,9 +19,9 @@ function App() {
 
       <BrowserRouter>
       <dataContext.Provider value={dataObj}>
-      <Header/>
+      
       <Main/>
-      <Footer/>
+      
       </dataContext.Provider>
       </BrowserRouter>
     </div>
